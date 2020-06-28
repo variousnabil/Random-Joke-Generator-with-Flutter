5845c15558b909d2f232c4874f73888e3b7dad18
variousnabil <varionabilr@gmail.com>
2020-06-28 13:27:47 +0700

    update with new api v2

--- lib/services/joke_api.dart
+++ lib/services/joke_api.dart
@@ -8,13 +8,15 @@ class JokeApi {
   String joke;
 
   Future<void> getJoke(String url) async {
-    Response response = await get('https://sv443.net/jokeapi/category/$url');
+    Response response = await get('https://sv443.net/jokeapi/v2/joke/$url');
     Map data = jsonDecode(utf8.decode(response.bodyBytes));
     print(data);
 
     id = data['id'];
     category = data['category'];
     type = data['type'];
-    joke = type == 'single' ? data['joke'] : data['setup'] + '\n' + data['delivery'];
+    joke = type == 'single'
+        ? data['joke']
+        : data['setup'] + '\n' + data['delivery'];
   }
 }
